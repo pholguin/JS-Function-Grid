42002e0e154f0a5d81601c4ef0e55e44838d4281
Pamela Holguin <pholguin.student@hsfi.us>
2017-01-17 21:44:39 +0000

    i made an S and a smily face

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//Write your function calls below this line
+//Write your function calls below this line]
+/*
+setColor (7,7, "blue");
+setColor (7,6, "purple");
+setColor (6,8, "purple");
+setColor(7,5, "blue");
+setColor(7,4, "purple");
+setColor(7,3, "blue");
+setColor(7,2, "purple");
+setColor(6,1, "blue");
+setColor(2,3, "green");
+setColor(2,6, "green");
+*/
+/*
+setColor(2,6, "black");
+setColor(2,5, "black");
+setColor(2,4, "black");
+setColor(3,3, "black");
+setColor(4,4, "black");
+setColor(4,5, "black");
+setColor(5,6, "black");
+setColor(6,5, "black");
+setColor(6,4, "black");
+setColor(6,3, "black");
+*/
